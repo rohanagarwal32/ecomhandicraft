f0427a00f5477289ccdddcd68ae7c3ea04a01577
ROHAN <agarwalrohan132@gmail.com>
2022-10-14 21:05:55 +0530

    admin panel

---
+++ admin/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <div class="footer-left">
+        <span>&copy;<?php echo date('Y'); ?> Handicraft Admin. All Rights Reserved.</span>
+    </div>
+    <div class="footer-right">
+        <span>Designed by: <a href="http://127.0.0.1:8000/home">HANDICRAFT</a></span>
+    </div>
+</div>
